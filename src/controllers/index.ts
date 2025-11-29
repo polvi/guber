ad038c2b78c09d53bc88d861cfe0e1afc8c0699c
Alex Polvi <alex@polvi.net>
2025-11-29 14:09:43 -0800

    refactor: remove unused cloudflare and github controllers

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,11 +1,7 @@
 import { Hono } from "hono";
-import cloudflare from "./cloudflare";
-import github from "./github";
 
 export function registerControllers(app: Hono<any>) {
   // Register cloudflare controller
   // Register github controller
   // No middleware registration needed - we use post-processing hooks
 }
-
-export { cloudflare, github };
